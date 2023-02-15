d9277daf2bb9fef3e1d3a3a14ca65f3346dd7273
beautifulseoul <117000041+beautifulseoul@users.noreply.github.com>
2023-02-16 00:06:40 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.01 ms, Memory: 75.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120818. 옷가게 할인 받기/옷가게 할인 받기.java
@@ -0,0 +1,7 @@
+class Solution {
+    public int solution(int price) {
+        int discountRate = price >= 500000 ? 20 : ((price >= 300000)? 10 : ((price >= 100000)? 5 : 0));
+        
+        return price * (100 - discountRate) / 100;
+    }
+}
