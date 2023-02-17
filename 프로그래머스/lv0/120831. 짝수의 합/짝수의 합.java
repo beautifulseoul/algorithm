d24b8c4468cd858f834dd975a02c4be4e7356d25
beautifulseoul <117000041+beautifulseoul@users.noreply.github.com>
2023-02-18 01:26:20 +0900

    [level 0] Title: 짝수의 합, Time: 0.02 ms, Memory: 78.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120831. 짝수의 합/짝수의 합.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solution(int n) {
+        
+        int totalNum = 0;
+        
+        for(int i = 1; i <= n; i++) {
+            if(i % 2 == 0) {
+                totalNum +=i;
+            }
+        }
+        
+        return totalNum;
+    }
+}
