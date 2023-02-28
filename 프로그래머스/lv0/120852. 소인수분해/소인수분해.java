90aa2399e0e6f8cffc50e7dd1cc125a25bb9af92
beautifulseoul <117000041+beautifulseoul@users.noreply.github.com>
2023-02-28 22:12:53 +0900

    [level 0] Title: 소인수분해, Time: 0.15 ms, Memory: 81.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120852. 소인수분해/소인수분해.java
+++ 프로그래머스/lv0/120852. 소인수분해/소인수분해.java
@@ -1,31 +1,25 @@
-import java.util.Arrays;
+import java.util.List;
+import java.util.ArrayList;
 
 class Solution {
     public int[] solution(int n) {
-        int[] intArr = new int[15];
-        int num = 0;
+        List<Integer> intList = new ArrayList<>();
+        
         for(int i = 2; i <= n; i++) {
             if(n % i == 0) {
                 int count = 0;
                 for(int j = 1; j < (int)Math.sqrt(i) + 1; j ++) {
-                    if(i % j == 0) {
-                        count ++;
-                    }
+                    count = (i % j == 0)? count + 1 : count; 
                 }
                 if(count == 1) {
-                    intArr[num] = i;
-                    num++;
+                    intList.add(i);
                 }
             }
         }
 
-        int[] answer = {};
-        for(int i = 0; i < intArr.length; i++) {
-            if(intArr[i] == 0) {
-                answer = Arrays.copyOfRange(intArr, 0, i);
-                break;
-
-            }
+        int[] answer = new int[intList.size()];
+        for(int i = 0; i < intList.size(); i++) {
+            answer[i] = intList.get(i);
         }
 
         return answer;
