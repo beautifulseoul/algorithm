f50cacde9831681da058828e1c0af3dddb11d499
beautifulseoul <117000041+beautifulseoul@users.noreply.github.com>
2023-02-28 23:17:14 +0900

    [level 0] Title: 컨트롤 제트, Time: 0.16 ms, Memory: 78 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120853. 컨트롤 제트/컨트롤 제트.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int solution(String s) {
+        String[] strArr = s.split(" ");
+        
+        int answer = 0;
+        for(int i = 0; i < strArr.length; i++) {
+            answer = (strArr[i].equals("Z"))? answer - Integer.valueOf(strArr[i - 1]) 
+                : answer + Integer.valueOf(strArr[i]);
+            
+            // if(strArr[i].equals("Z")) {
+            //    answer -= Integer.valueOf(strArr[i - 1]);
+            // } else {
+            //     answer += Integer.valueOf(strArr[i]);
+            // }
+            
+        }
+        
+        
+        return answer;
+    }
+}
