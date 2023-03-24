682193b9c3630897e37b003074744db6966fc30f
beautifulseoul <117000041+beautifulseoul@users.noreply.github.com>
2023-03-25 01:40:43 +0900

    [level 0] Title: 안전지대, Time: 0.52 ms, Memory: 71.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120866. 안전지대/안전지대.java
+++ 프로그래머스/lv0/120866. 안전지대/안전지대.java
@@ -3,7 +3,6 @@ class Solution {
         
         int[][] newArr = new int[board.length][board[0].length];
         
-        // 지뢰 중심
         for(int i = 0; i < board.length; i++) {
             for(int j = 0; j < board[i].length; j++) {
                 if(board[i][j] == 1) {
@@ -20,7 +19,6 @@ class Solution {
         for(int i = 0; i < board.length; i++) {
             for(int j = 0; j < board[i].length; j++) {
                 if(newArr[i][j] == 0) count++;
-                System.out.println(newArr[i][j]);
             }
         }
         return count;
