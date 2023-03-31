f5d5887f44e4e0cd6e9d5c07f77c43385bb9a440
beautifulseoul <117000041+beautifulseoul@users.noreply.github.com>
2023-03-31 12:36:42 +0900

    [level 0] Title: 삼각형의 완성조건 (2), Time: 0.34 ms, Memory: 75.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120868. 삼각형의 완성조건 （2）/삼각형의 완성조건 （2）.java
@@ -0,0 +1,33 @@
+import java.util.Arrays;
+ import java.util.Set;
+ import java.util.HashSet;
+
+class Solution {
+    public int solution(int[] sides) {
+        
+        Arrays.sort(sides);
+        Set set = new HashSet();
+        
+        // 가장 짧은 변
+        for(int i = 1 ; i <= sides[0]; i++) {
+            if(i > sides[1] - sides[0]) {
+            set.add(i);
+            }
+        }
+        
+        // 중간길이 변        
+        for(int i = sides[0]; i <= sides[1]; i++) {
+            if(i > sides[1] - sides[0]) {
+                set.add(i);
+            }
+        }
+        
+        // 가장 긴 변
+        for(int i = sides[1]; i < sides[0] + sides[1] ; i++) {
+            set.add(i);
+        }
+        
+        int answer = 0;
+        return set.size();
+    }
+}
