8d70d961944056032621760f0c8c4b4200a2428b
beautifulseoul <117000041+beautifulseoul@users.noreply.github.com>
2023-04-04 03:03:21 +0900

    [level 0] Title: 겹치는 선분의 길이, Time: 0.07 ms, Memory: 73.8 MB -BaekjoonHub

--- 프로그래머스/lv0/120876. 겹치는 선분의 길이/겹치는 선분의 길이.java
+++ 프로그래머스/lv0/120876. 겹치는 선분의 길이/겹치는 선분의 길이.java
@@ -8,21 +8,11 @@ class Solution {
         int count = 0;
         HashMap<Integer, Integer> map = new HashMap();
         
-        // 첫번째 선분
-        for(int i = lines[0][0]; i < lines[0][1]; i++) {
-            map.put(i, 1);
-        }
-        
-        // 두번째 선분
-        for(int i = lines[1][0]; i < lines[1][1]; i++) {
-            if(map.get(i) == null) { map.put(i, 1); }
-            else { map.put(i, map.get(i) + 1); }
-        }
-        
-        // 세번째 선분
-        for(int i = lines[2][0]; i < lines[2][1]; i++) {
-            if(map.get(i) == null) { map.put(i, 1); }
-            else { map.put(i, map.get(i) + 1); }
+        for(int i = 0; i < lines.length; i++) {
+            for(int j = lines[i][0]; j < lines[i][1]; j++) {
+                if(map.get(j) == null) { map.put(j, 1); }
+                else { map.put(j, map.get(j) + 1); }
+            }
         }
 
         Set s = map.entrySet();
@@ -32,7 +22,7 @@ class Solution {
             Map.Entry e = (Map.Entry)it.next();            
             if((int)e.getValue() != 1) count++;
         }
-
+        
         return count;
     }
 }
