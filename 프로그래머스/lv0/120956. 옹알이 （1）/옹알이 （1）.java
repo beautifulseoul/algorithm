93501952305dcb05c18d5df5df3be9cce0348bf1
beautifulseoul <117000041+beautifulseoul@users.noreply.github.com>
2023-04-07 19:14:05 +0900

    [level 0] Title: 옹알이 (1), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120956. 옹알이 （1）/옹알이 （1）.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int solution(String[] babbling) {
+        
+        String[] wordArr = {"aya", "ye", "woo", "ma"};
+        int count = 0;
+        
+        for(int i = 0; i < babbling.length; i++) {
+            for(int j = 0; j < wordArr. length; j++) {
+                if(babbling[i].contains(wordArr[j])) {
+                    babbling[i] = babbling[i].replaceFirst(wordArr[j], " ");
+                }
+            }
+            System.out.println(babbling[i]);
+            if(babbling[i].replaceAll(" ", "").equals("")) count++;
+        }
+        return count;
+    }
+}
