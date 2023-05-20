474df95adc63ff32848fbd0282c734b19a357704
beautifulseoul <117000041+beautifulseoul@users.noreply.github.com>
2023-05-21 00:22:43 +0900

    [level 0] Title: 다음에 올 숫자, Time: 0.02 ms, Memory: 67.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120924. 다음에 올 숫자/다음에 올 숫자.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int solution(int[] common) {
+        
+        if(common[2] - common[1] == common[1] - common[0]) {
+            // 등차수열 (공차 : common difference)
+            return common[0] + common.length * (common[1] - common[0]);            
+        } else {
+            // 등비수열 (공비 : common ratio)
+            int commonRatio = common[1] / common[0];
+            int answer = common[0];
+            for(int i = 0; i < common.length; i++) {
+                answer *= commonRatio;
+            }
+            return answer;
+        }
+    }
+}
