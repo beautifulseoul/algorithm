c7049c194d2d2fd895be6737e6ddf487dbd4300b
beautifulseoul <117000041+beautifulseoul@users.noreply.github.com>
2023-05-22 20:15:53 +0900

    [level 0] Title: 문자열 밀기, Time: 0.06 ms, Memory: 70.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120921. 문자열 밀기/문자열 밀기.java
+++ 프로그래머스/lv0/120921. 문자열 밀기/문자열 밀기.java
@@ -3,7 +3,6 @@ import java.util.Arrays;
 class Solution {
     public int solution(String A, String B) {
         
-        int count = 0;
         int length = A.length();
         char[] aArr = A.toCharArray();
         StringBuilder sb = new StringBuilder();
@@ -11,13 +10,12 @@ class Solution {
         if(A.equals(B)) return 0;
         
         for(int j = 0; j < length - 1; j++) {
-            count++;
             sb.setLength(0);
             
             sb.append(new String(aArr, length - j - 1, j + 1));        
             sb.append(new String(aArr, 0, length - j - 1)); 
             
-            if(sb.toString().equals(B)) return count;
+            if(sb.toString().equals(B)) return j + 1;
         }
 
     return -1;
