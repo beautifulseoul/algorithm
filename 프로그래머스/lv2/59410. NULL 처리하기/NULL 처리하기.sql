0dce868d850971dcd62c8415f6db40d492564c08
beautifulseoul <117000041+beautifulseoul@users.noreply.github.com>
2023-06-12 01:26:04 +0900

    [level 2] Title: NULL 처리하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv2/59410. NULL 처리하기/NULL 처리하기.sql
+++ 프로그래머스/lv2/59410. NULL 처리하기/NULL 처리하기.sql
@@ -1,6 +1,5 @@
--- 코드를 입력하세요
 SELECT animal_type, 
-        CASE WHEN name IS NULL THEN 'No name' ELSE name END, 
+        CASE WHEN name IS NULL THEN 'No name' ELSE name END AS name, 
         sex_upon_intake
 FROM animal_ins
 ORDER BY animal_id
