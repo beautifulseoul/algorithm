8d54abe45fe602b28c7386d1a615a3cc50b2dc57
beautifulseoul <117000041+beautifulseoul@users.noreply.github.com>
2024-04-28 09:11:40 +0900

    [level 0] Title: 문자열 겹쳐쓰기, Time: 0.09 ms, Memory: 84.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.java
@@ -0,0 +1,9 @@
+class Solution {
+    public String solution(String my_string, String overwrite_string, int s) {
+        
+        StringBuffer sb = new StringBuffer(my_string);
+        sb.replace(s, s + overwrite_string.length(), overwrite_string);
+        String answer = sb.toString();
+        return answer;
+    }
+}
