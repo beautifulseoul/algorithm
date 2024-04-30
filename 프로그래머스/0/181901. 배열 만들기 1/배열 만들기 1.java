983628083c0ffbc90b5a81743c7672c7b15dcf57
beautifulseoul <117000041+beautifulseoul@users.noreply.github.com>
2024-04-30 21:59:46 +0900

    [level 0] Title: 배열 만들기 1, Time: 0.03 ms, Memory: 76.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181901. 배열 만들기 1/배열 만들기 1.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int[] solution(int n, int k) {
+        
+        int num = n / k;
+        int[] answer = new int[num];
+        
+        for(int i = 0; i < num; i++) {
+            answer[i] = (i + 1) * k;
+        }        
+        return answer;
+    }
+}
